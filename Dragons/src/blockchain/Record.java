9b7c813022435c7d7eb0ae545f1896ffb2ce5122
josejpalacios <jose_palacios61@yahoo.com>
2017-09-14 20:12:33 -0500

    Did something

--- Dragons/src/blockchain/Record.java
+++ Dragons/src/blockchain/Record.java
@@ -2,13 +2,13 @@ package blockchain;
 
 public class Record {
 		
-	public String firstName;
-	public String lastName;
-	public int SSN;
-	public String signedSHA256;
-	public int blockID;
-	public int verificationProcessID;
-	public String SHA256String;
+	private String firstName;
+	private String lastName;
+	private int SSN;
+	private String signedSHA256;
+	private int blockID;
+	private int verificationProcessID;
+	private String SHA256String;
 	
 	public Record next;
 	
@@ -26,5 +26,62 @@ public class Record {
 	
 	public Record() {
 	}
+	
+	//Setters
+	public void setFirstName(String fName) {
+		this.firstName = fName;
+	}
+	
+	public void setLastName(String lName) {
+		this.lastName = lName;
+	}
+	
+	public void setSSN(int ssn) {
+		this.SSN = ssn;
+	}
 		
+	public void setSignedSHA256(String signedSHA) {
+		this.signedSHA256 = signedSHA;
+	}
+	
+	public void setBlockId(int bID) {
+		this.blockID = bID;
+	}
+	
+	public void setVerificationProcessID(int vpID) {
+		this.verificationProcessID = vpID;
+	}
+	
+	public void setSHA256String(String shaString) {
+		this.SHA256String = shaString;
+	}
+	
+	//Getters 
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	
+	public int getSSN() {
+		return this.SSN;
+	}
+	
+	public String getSignedSHA256() {
+		return this.signedSHA256;
+	}
+	
+	public int getBlockId() {
+		return this.blockID;
+	}
+	
+	public int getVerificationProcessID() {
+		return this.verificationProcessID;
+	}
+	
+	public String getSHA256String() {
+		return this.SHA256String;
+	}
 }
