04f367a7fae2facc8e4de09d61e2ab8cd633ffdd
Karen <karenhdez2017@gmail.com>
2017-09-14 19:36:34 -0500

    Added print and getListByPatient functionality.

--- Dragons/src/blockchain/Blockchain.java
+++ Dragons/src/blockchain/Blockchain.java
@@ -8,41 +8,40 @@ public class Blockchain {
 	private LinkedList<Record> list;
 	
 	public Blockchain() {
-		
+		list = new LinkedList<Record>();
 	}
 	
 	public void addRecord(Record r) {
-		
+		list.add(r);
 	}
 	
 	public LinkedList<Record> getList() {
-		
-		return new LinkedList<Record>();
-		
+		return list;
 	}
 	
 	public LinkedList<Record> getListByPatient(String name) {
-		
-		return new LinkedList<Record>();
-		
+		LinkedList<Record> l = list;
+		LinkedList<Record>  listWithName = new LinkedList<Record>();
+		for(int i = 0; i < l.size(); i++) {
+			if (l.get(i).getFirstName().equals(name)) {
+				listWithName.add(l.get(i));
+			}
+		}
+		return listWithName;
 	}
 	
+	//TODO
 	public LinkedList<Record> getListByProvider(String provider) {
-		
-		return new LinkedList<Record>();
-		
+		return list;
 	}
 	
+	//TODO
 	public LinkedList<Record> getListByDateWindow(Date start, Date end) {
-		
 		return new LinkedList<Record>();
-		
 	}
 	
 	public void print() {
-		
-		System.out.println("");
-		
+		System.out.println("Records: ");
+		System.out.println(list);
 	}
-
 }
--- Dragons/src/blockchain/Record.java
+++ Dragons/src/blockchain/Record.java
@@ -1,30 +1,63 @@
 package blockchain;
 
 public class Record {
-		
 	public String firstName;
 	public String lastName;
 	public int SSN;
-	public String signedSHA256;
+	public String provider;
 	public int blockID;
-	public int verificationProcessID;
 	public String SHA256String;
+	public String signedSHA256;
+	public int verificationProcessID;
 	
 	public Record next;
 	
-	public String generateString(int size) {
-		
-		return "";
+	public Record(String f, String l, int ssn, String p) {
+		this.firstName = f;
+		this.lastName = l;
+		this.SSN = ssn;
+		this.provider = p;
+		this.blockID = this.generateBlockID();
+		this.SHA256String = this.generateSHA256();
+		this.signedSHA256 = this.generateSignedSHA256();
+		this.verificationProcessID = this.generateProcessId();
+	}
+	
+	//Temporary: for now generates random string
+	public String generateSHA256() {
+		String sHA256 = "WELKFRLEA";
+		return sHA256;
 		
 	}
 	
-	public int generateID(int size) {
-		
-		return 0;
+	//Temporary: for now generates random id
+	//TODO: This can be fixed now
+	public int generateBlockID() {
+		return 1;
 		
 	}
 	
-	public Record() {
+	//Temporary: for now generates a random string representation of the signed SHA256
+	public String generateSignedSHA256(){
+		String signedSHA256 = "LKEFALEKFALEK";
+		return signedSHA256;
 	}
-		
+	
+	//TODO: check how process Id is selected and used
+	public int generateProcessId() {
+		return 2;
+	}
+	
+	//TODO: Add more attributes to return
+	public String toString(){
+		return "Name: " + this.firstName + " " + this.lastName;
+	}
+	
+	public Record getNext() {
+		return next;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
 }
