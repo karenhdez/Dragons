3d6be68a7e05f93be52095bec01fdde55aa8f874
Eric Lynch <lynch.er18@gmail.com>
2017-09-15 00:11:59 -0500

    Input interface

--- Dragons/src/blockchain/Main.java
+++ Dragons/src/blockchain/Main.java
@@ -7,38 +7,85 @@ import java.io.InputStreamReader;
 public class Main {
 
 	private static boolean quit = false;
-	
-	public static void main(String[] args) {
+	private static InputInterface currentInput = Main::inputOptions;
+
+	private static int getInteger(BufferedReader br) {
 		
-		while(!quit) {
-			
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String s = "";
-			int i;
-			
-			/*System.out.println("Enter Integer:");
-	        try {
-				i = Integer.parseInt(br.readLine());
-	        }
-	        catch(NumberFormatException nfe){
-	            System.err.println("Invalid Format!");
-	        }
-	        catch (IOException e) {
+		while(true) {
+			try {
+				int i = Integer.parseInt(br.readLine());
+				return i;
+		    }
+		    catch (NumberFormatException nfe) {
+		        System.err.println("Invalid Format!");
+		    }
+		    catch (IOException e) {
 				e.printStackTrace();
-			}*/
-			
-			System.out.println("Enter String or enter q to quit:");
-	        try {
-				s = br.readLine();
+			}
+		}
+		
+	}
+	
+	private static String getString(BufferedReader br) {
+		
+		while(true) {
+			try {
+				String s = br.readLine();
+				return s;
 			} 
 	        catch (IOException e1) {
 				e1.printStackTrace();
 			}
-	        
-	        quit = s.equals("q");
-			
 		}
 		
 	}
+	
+	private static void inputRecord(BufferedReader br) {
+    	
+    	System.out.println("Enter record info.");
+    	System.out.println("Enter q to quit.");
+    	
+    	String s = getString(br);
+    	
+    	if (s.equals("q"))
+    		currentInput = Main::inputOptions;
+        
+    }
+	
+    private static void inputOptions(BufferedReader br) {
+    	
+    	System.out.println("Select an option.");
+    	System.out.println("0. Quit");
+    	System.out.println("1. Add record");
+    	
+    	int i = getInteger(br);
+    	
+    	switch (i) {
+	        case 0:
+	        	quit = true;
+	        	break;
+	        case 1:
+	        	currentInput = Main::inputRecord;
+	            break;
+    	}
+        
+    }
+
+    @FunctionalInterface
+    public static interface InputInterface{
+        void input(BufferedReader br);
+    }
+	
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+        while(!quit) {
+        	
+        	currentInput.input(br);
+        	
+        }
+        
+    }
 
 }
