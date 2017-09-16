aadaf084e020b695cef125c1ffad2e13038c9c51
josejpalacios <jose_palacios61@yahoo.com>
2017-09-16 15:52:39 -0500

    Reduced code and moved code/method to other files.

--- Dragons/src/server/Server.java
+++ Dragons/src/server/Server.java
@@ -12,56 +12,57 @@ import blockchain.Blockchain;
 public class Server {
 
 	public static void main(String[] args) throws IOException {
-		Blockchain block = new Blockchain();
+		System.out.println("Waiting for a connection.");
 		try (
-	            ServerSocket serverSocket = new ServerSocket(6019);
-	            Socket clientSocket = serverSocket.accept();
-	            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
-	            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//Reads from socket
-	        ) {
-				System.out.println("Connection made");
-				while(true) {
-					String clientOption = in.readLine();
-					if(clientOption.equals("N") || clientOption.equals("n")) {
-						String firstName = in.readLine();
-						String lastName = in.readLine();
-						int ssn = Integer.parseInt(in.readLine());
-						String provider = in.readLine();
-						Record clientRecord = generateClientRecord(firstName, lastName, ssn, provider);
-						
-						System.out.println("===================================");
-						System.out.println("Creating Record:");
-						System.out.println("  First Name: " + firstName);
-						System.out.println("  Last Name: " + lastName);
-						System.out.println("  SSN: " + ssn);
-						System.out.println("  Provider: " + provider);
-						System.out.println("===================================");
-						
-						block.addRecord(clientRecord);
-					}
-					else if(clientOption.equals("G") || clientOption.equals("g")) {
-						String name = in.readLine();
-						System.out.println("===================================");
-						System.out.println("Requested Info: " + name);
-						System.out.println("Info: " + block.getListByPatient(name));
-						System.out.println("===================================");
-						System.out.println();
-						out.println(block.getListByPatient(name));
-					}
-					else if(clientOption.equals("Q") || clientOption.equals("q")) {
-						in.close();
-						out.close();
-						clientSocket.close();
-						serverSocket.close();
+				ServerSocket serverSocket = new ServerSocket(6019);
+				Socket clientSocket = serverSocket.accept();
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//Reads from socket
+				) {
+			Blockchain block = new Blockchain();
+			HandleClientOperations handle = new HandleClientOperations();
+
+			System.out.println("Connection made");
+			while(true) {
+				String clientStartOption = in.readLine();
+				System.out.println("Start Option: " + clientStartOption);
+				if(clientStartOption.equals("L") || clientStartOption.equals("l")) {
+					handle.Login(in, out);
+					while (true) {
+						String clientMainMenuOption = in.readLine();
+						System.out.println("Main Menu Option: " + clientMainMenuOption);
+						if(clientMainMenuOption.equals("N") || clientMainMenuOption.equals("n")) {
+							Record clientRecord = handle.NewRecord(in, out);
+							block.addRecord(clientRecord);
+						}
+						else if(clientMainMenuOption.equals("G") || clientMainMenuOption.equals("g")) {
+							String clientGetRecordInfoMenuOption = in.readLine();
+							System.out.println("Get Info Option: " + clientGetRecordInfoMenuOption);
+							if(clientGetRecordInfoMenuOption.equals("1")) {
+								handle.GetRecordInformationByPatientName(in, out, block);
+							}
+							else if(clientGetRecordInfoMenuOption.equals("2")) {
+								handle.GetRecordInformationByProvider(in, out, block);
+							}
+							else if(clientGetRecordInfoMenuOption.equals("B") || clientGetRecordInfoMenuOption.equals("b")) {
+								continue;
+							}
+
+						}
+						else if(clientMainMenuOption.equals("L") || clientMainMenuOption.equals("l")) {
+							break;
+						}
 					}
 				}
-	        } catch (IOException e) {
-	            System.out.println(e.getMessage());
-	        }
+				else if (clientStartOption.equals("Q") || clientStartOption.equals("q")) {
+					in.close();
+					out.close();
+					clientSocket.close();
+					serverSocket.close();
+				}
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
-	
-	private static Record generateClientRecord(String firstName, String lastName, int ssn, String provider) {
-		return new Record(firstName, lastName, ssn, provider);
-	}
-
 }
