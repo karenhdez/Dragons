1c1183280e45ae4ce3e8fe425f4babcf1391cc6a
Eric Lynch <lynch.er18@gmail.com>
2017-09-21 17:12:34 -0500

    All python but not functional

--- src/server/Server.py
+++ src/server/Server.py
@@ -1,49 +1,44 @@
-package server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import blockchain.Record;
-import blockchain.Blockchain;
 
 public class Server {
 
-	public static void main(String[] args) throws IOException {
-		Blockchain block = new Blockchain();
-		try (
-	            ServerSocket serverSocket = new ServerSocket(6019);
-	            Socket clientSocket = serverSocket.accept();
-	            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
-	            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//Reads from socket
-	        ) {
-				System.out.println("Connection made");
-	            String clientOption = in.readLine();
-	            if(clientOption.equals("N") || clientOption.equals("n")) {
-	            	String firstName = in.readLine();
-	            	String lastName = in.readLine();
-	            	int ssn = Integer.parseInt(in.readLine());
-	            	String provider = in.readLine();
-	            	Record clientRecord = generateClientRecord(firstName, lastName, ssn, provider);
-	            	block.addRecord(clientRecord);
-	            	System.out.println(block.getListByPatient(firstName));
-	            }
-	            else if(clientOption.equals("Q") || clientOption.equals("q")) {
-	            	in.close();
-	            	out.close();
-	            	clientSocket.close();
-	            	serverSocket.close();
-	            }
-	            
-	        } catch (IOException e) {
-	            System.out.println(e.getMessage());
-	        }
-	}
-	
-	private static Record generateClientRecord(String firstName, String lastName, int ssn, String provider) {
-		return new Record(firstName, lastName, ssn, provider);
-	}
+
+    def __init__(self):
+
+        pass
+
+
+    def generateClientRecord(self, firstName, lastName, ssn, provider):
+
+        return Record(firstName, lastName, ssn, provider)
+
+
+	def main(self, String[] args):
+
+		Blockchain block = Blockchain()
+		try:
+            _serverSocket = ServerSocket(6019)
+            _clientSocket = serverSocket.accept()
+            _out = PrintWriter(clientSocket.getOutputStream(), True)
+            _in = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+
+            print("Connection made")
+            clientOption = in.readLine()
+
+            if clientOption.equals("N") or clientOption.equals("n"):
+                firstName = in.readLine()
+                lastName = in.readLine()
+                ssn = Integer.parseInt(in.readLine())
+                provider = in.readLine()
+                clientRecord = self.generateClientRecord(firstName, lastName, ssn, provider)
+                block.addRecord(clientRecord)
+                print(block.getListByPatient(firstName))
+            elif clientOption.equals("Q") or clientOption.equals("q"):
+                in.close()
+                out.close()
+                clientSocket.close()
+                serverSocket.close()
+	    catch:
+	        print()
 
 }
