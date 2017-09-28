87b0344db642eb21c390d08902f8fe907efa6de7
Eric Lynch <lynch.er18@gmail.com>
2017-09-28 16:49:46 -0500

    Merge branch 'master' into Eric
    # Conflicts:
    #	Dragons/src/blockchain/Record.java

--- src/blockchain/Record.py
+++ src/blockchain/Record.py
@@ -1,101 +1,34 @@
+import datetime
+from ..Person import Doctor
+from ..Person import Patient
 
-
-from System import *
-from System.Collections.Generic import *
-from System.Collections import *
-from System.sql.Timestamp import *
-from System.text.SimpleDateFormat import *
 
 class Record:
 
-	def __init__(self, f, l, ssn, p):
+    def __init__(self, f, l, ssn, p):
+
         self._next = None
 
-		self._firstName = f
-		self._lastName = l
-		self._SSN = ssn
-		self._provider = p
-		self._blockID = self.generateBlockID()
-		self._SHA256String = self.generateSHA256()
-		self._signedSHA256 = self.generateSignedSHA256()
-		self._verificationProcessID = self.generateProcessId()
-		self._timestamp = Timestamp(System.currentTimeMillis())
+        self.firstName = f
+        self.lastName = l
+        self.SSN = ssn
+        self.provider = p
+        self.timestamp = str(datetime.datetime.now()).split('.')[0]
 
-    # Temporary: for now generates random string
-    def generateSHA256(self):
-        sHA256 = "WELKFRLEA"
-        return sHA256
 
-    # Temporary: for now generates random id
-    # TODO: This can be fixed now
-    def generateBlockID(self):
-        return 1
+    # TODO: Add more attributes to return
+    def toString(self):
 
-    # Temporary: for now generates a random string representation of the signed SHA256
-    def generateSignedSHA256(self):
-        signedSHA256 = "LKEFALEKFALEK"
-        return signedSHA256
+        return ""
 
-    # Setters
-    def setFirstName(self, fName):
-        self._firstName = fName
-
-    def setLastName(self, lName):
-        self._lastName = lName
-
-    def setSSN(self, ssn):
-        self._SSN = ssn
-
-    def setSignedSHA256(self, signedSHA):
-        self._signedSHA256 = signedSHA
-
-    def setBlockId(self, bID):
-        self._blockID = bID
-
-    def setVerificationProcessID(self, vpID):
-        self._verificationProcessID = vpID
-
-    def setSHA256String(self, shaString):
-        self._SHA256String = shaString
-
-    #Getters
-	def getSSN(self):
-		return self._SSN
-
-	def getSignedSHA256(self):
-		return self._signedSHA256
-
-    def getBlockId(self):
-		return self._blockID
-
-	def getVerificationProcessID(self):
-		return self._verificationProcessID
-
-	def getSHA256String(self):
-		return self._SHA256String
-
-    #TODO: check how process Id is selected and used
-	def generateProcessId(self):
-		return 2
-
-	#TODO: Add more attributes to return
-	def toString(self):
-		return "Name: " + self._firstName + " " + self._lastName + " Provider: " + self.provider + " Time: " + self.timestamp
-
-    def getNext(self):
-        return self._next
-
-    def getFirstName(self):
-        return self._firstName
-
-    def getLastName(self):
-        return self._lastName
-
-    def getProvider(self):
-        return self._provider
-
-    def getTimestamp(self):
-        return self._timestamp
 
     def setTimestamp(self, ts):
-        self._timestamp = ts
+
+        self.timestamp = ts
+
+
+    def getTimestamp(self):
+
+        return self.timestamp
+
+
