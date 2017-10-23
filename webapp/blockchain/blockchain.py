9c198d031913dfcb964d397de40da8c97a1adf4a
Daniel Saldivar-Salas <daniel.saldivar.ds@gmail.com>
2017-10-23 14:19:18 -0500

    added network for the blockchain using Flask

--- webapp/blockchain/blockchain.py
+++ webapp/blockchain/blockchain.py
@@ -1,24 +1,111 @@
-from Block import Block
-import datetime
-
+from block import Block
+import time
+import hashlib
+import json
 
 class Blockchain:
 
-    def create_genesis_block(self):
-        '''The first block of the blockchain.
-        We will start the index at 0. '''
-        return Block(0, datetime.datetime.now(), "Genesis Block","0")
+    def __init__(self):
+        self.current_patient_events = []
+        self.blockchain = []
 
-    def next_block(last_block):
-        next_block_index = last_block.index + 1 #increment index by 1
-        next_block_timestamp = datetime.datetime.now()
-        next_block_data = "Hello my block ID is " + str(next_block_index) #TODO this data will be in json
-        next_block_hash = last_block.hash()
-        return Block(next_block_index, next_block_timestamp, next_block_data, next_block_hash)
+        #Add genesis block to the blockchain
+        self.add_block(previous_hash=1, proof=100)
+
+    def add_patient_event(self, patient_id, timestamp, ssn, name, provider):
+        '''
+        This function will add a new patient event into the current list of
+        patient events to be mined into the next Block
+
+        :param patient_id:
+        :param ssn:
+        :param name:
+        :param provider:
+        :return:
+        '''
+
+        self.current_patient_events.append({
+            'patient_id': patient_id,
+            'timestamp': timestamp,
+            'ssn': ssn,
+            'name': name,
+            'provider': provider
+        })
+
+        return self.last_block['index'] + 1
+    
+    def add_block(self, proof, previous_hash=None):
+        '''
+        Add a new Block into the Blockchain.
 
 
-honeybadgercoin = Blockchain()
+        :param last_block:
+        :param data:
+        :return:
+        '''
 
-#Start Genesis Block
-honeybadgercoin.create_genesis_block()
+        block = {
+            'index': len(self.blockchain) + 1,
+            'timestamp': time.time(),
+            'patient_events': self.current_patient_events,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash_block(self.blockchain[-1])
+        }
+
+        # Reset the current list of patient events array
+        self.current_patient_events = []
+
+        self.blockchain.append(block)
+
+        return block
+
+    @staticmethod
+    def hash_block(block):
+        '''
+        Hashes a block using SHA-256
+        :param block:
+        :return:
+        '''
+
+
+        serialized_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(serialized_block).hexdigest()
+
+    @property
+    def last_block(self):
+        '''
+        Returns the last block in the chain
+        '''
+        return self.blockchain[-1]
+    
+    def is_blockchain_valid(self):
+        pass
+
+    @staticmethod
+    def proof_of_work(block):
+        '''
+        Hashcash proof of work algorithms will find a nonce such that the hash of the nonce and block
+        will give us 4 leading zeros within our new hash.
+
+        This will also prove as valid proof ofwork
+        :param block:
+        :return:
+        '''
+        complete = False
+        proof = 0
+
+        serialized_block = json.dumps(block, sort_keys=True).encode()
+
+        while complete == False:
+            proof = proof + 1
+            sha = hashlib.sha256()
+            sha.update(serialized_block + str(proof).encode())
+            curr_hash = sha.hexdigest()
+
+            # slows performance drastically
+            ## print curr_hash
+
+            if curr_hash.startswith('00000'):
+                complete = True
+        return proof, curr_hash
 
---
+++ webapp/blockchain/network.py
@@ -0,0 +1,73 @@
+from blockchain import Blockchain
+
+import hashlib
+import json
+
+import time
+from uuid import uuid4
+
+from flask import Flask, request, jsonify
+import requests
+
+
+app = Flask(__name__)
+
+#this will create a random name for the node
+node_identifier = str(uuid4()).replace('-', '')
+
+dragoncoin = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    proof, hashed_block = dragoncoin.proof_of_work(dragoncoin.last_block)
+    results = {"message": "New block wasadded to the blockchain",
+
+                "proof": proof,
+                "hashed_block": hashed_block}
+               
+    return jsonify(results), 201
+
+@app.route('/patient_event/new', methods=['POST'])
+def new_patient_event():
+    ''' Post values should be: 
+    patient_id
+    timestamp
+    SSN
+    name
+    provider
+    '''
+    values = request.get_json(force=True)
+    
+    #Check that the required fields were submitted by POST
+    required = ['patient_id', 'ssn', 'name', 'provider']
+    
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    #Create a new patient event
+    
+    index = dragoncoin.add_patient_event(values['patient_id'],
+                                        str(time.time()),
+                                        values['ssn'],
+                                        values['name'],
+                                        values['provider'])
+    
+  
+    tender = values
+    response = {'message': f'{tender} Patient event will be added to the Block {index}'}
+    
+    return jsonify(response), 201
+
+@app.route('/blockchain', methods=['GET'])
+def full_blockchain():
+    
+
+    response = {
+        'chain': dragoncoin.blockchain,
+        'length': len(dragoncoin.blockchain)
+    }
+    return jsonify(response)
+    
+if __name__ =='__main__':
+    app.run(debug=True,port=5000)
